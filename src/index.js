8e2704096292db3dcd165d391a9a41214a038236
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-05-07 20:11:51 +0300

    export global style

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from 'global/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>

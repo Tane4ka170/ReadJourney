936ea3eb3071bcecfcf35418c2676e5b342ee93b
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-05-07 20:11:09 +0300

    global styles and imgs

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/ReadJourney">
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+);
